467f602099d55c17c9e9b940419635c263481393
rjcremin <rjcremin@gmail.com>
2020-05-10 09:00:45 -0400

    p4 + p5 Push

---
+++ p5/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+ $(".frontal").hover(function(){
+   $("#container1").attr("background-image" , "../image/");
+ });
+});
+
+background-image: url(../image/brain_blank.png);
